8694b02aa7846a9634a399741d903bbeedf357f9
zhangxixi666 <517328263@qq.com>
2020-02-16 16:48:42 +0800

    interface modification分支

--- src/main/java/org/sang/branchTest/branchTest.java
+++ src/main/java/org/sang/branchTest/branchTest.java
@@ -2,6 +2,6 @@ package org.sang.branchTest;
 
 public class branchTest {
     public static void main(String[] args){
-        System.out.println("i am Interface modification");
+        System.out.println("i am branchTest");
     }
 }
